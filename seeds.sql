b5a3686d4bbad7e0817d77790e55590fc454ce9d
TJFitz <tjfitz@comcast.net>
2020-09-25 17:15:44 -0400

    combining things

--- seeds.sql
+++ seeds.sql
@@ -4,31 +4,28 @@ INSERT INTO Bookings (name, email, phone, qty, message) VALUES ("Steve and Compa
 
 INSERT INTO Menu (category) VALUES ("test menu");
 
-INSERT INTO Appetizers (name, description, price, tags, image, MenuId) VALUES ("Stuffed Mushrooms", "Hand-stuffed with garlic herb cheese, topped with Parmesan crust and served over a creamy blend of four melted cheeses.", 8.99, "730cal", "assets/images/menu1.jpg", 1);
+INSERT INTO Appetizers (name, description, price, tags, image, MenuId) VALUES ("Spring Avocado Toast", "Grilled sourdough bread, avocado, fresh greens", 8.99, "730cal", "https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 1);
 
-INSERT INTO Appetizers (name, description, price, tags, image, MenuId) VALUES ("Spicy Chicken Bites", "Fresh and tender hand-cut chicken tossed in a sweet and spicy chili-ginger sauce. Garnished with peppers and spices for an added kick.", 10.99, "920cal", "assets/images/menu2.jpg", 1);
+INSERT INTO Appetizers (name, description, price, tags, image, MenuId) VALUES ("Smoked Salmon Crisps", "Salmon cornets topped with creme fraiche and fresh salmon", 10.99, "920cal", "https://images.pexels.com/photos/41967/appetizer-canape-canapes-cheese-41967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 1);
 
-INSERT INTO Appetizers (name, description, price, tags, image, MenuId) VALUES ("Grilled Chicken Salad", "Fresh, crisp field greens with grapes, strawberries, mandarin oranges, candied pecans and feta cheese. All tossed up in a sweet raspberry vinaigrette.", 11.99, "540cal", "assets/images/menu3.jpg", 1);
-
-INSERT INTO Appetizers (name, description, price, tags, image, MenuId) VALUES ("Firecracker Wraps", "Spicy grilled chicken and cheese wrapped in a crispy flour tortilla, and served with a cool avocado-lime dipping sauce.", 9.99, "810cal", "assets/images/menu4.jpg", 1);
+INSERT INTO Appetizers (name, description, price, tags, image, MenuId) VALUES ("Sweet Potato Fries", "Hand-cut sweet potato fries with flavorful garnishes", 11.99, "540cal", "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 1);
 
 
 
-INSERT INTO Entrees (name, description, price, tags, image, MenuId) VALUES ("The Lockhart Burger", "Our thick, juicy half-pound burger grilled to order with lettuce, onion, tomato, bacon and housemade burger sauce on a toasted potato bun.", 15.99, "1020cal", "assets/images/menu5.jpg", 1);
+INSERT INTO Entrees (name, description, price, tags, image, MenuId) VALUES ("Lockhart Filet", "Tender-cut filet, hand-seasoned and cooked to perfection", 15.99, "1020cal", "https://images.pexels.com/photos/675951/pexels-photo-675951.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 1);
 
-INSERT INTO Entrees (name, description, price, tags, image, MenuId) VALUES ("Redrock Shrimp", "Grilled jumbo shrimp glazed with smoky tomato butter, served over rice with garlic butter. Served with brocolli and your choice of soup or salad.", 13.99, "940cal", "assets/images/menu6.jpg", 1);
+INSERT INTO Entrees (name, description, price, tags, image, MenuId) VALUES ("Cherry Tomato Pasta", "Angel hair pasta with fresh cherry tomatoes", 13.99, "940cal", "https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 1);
 
-INSERT INTO Entrees (name, description, price, tags, image, MenuId) VALUES ("Parmesan Chicken", "Our fresh, juicy, grilled chicken becomes irresistible when we top it with our creamy, crunchy Parmesan and garlic cheese crust.", 12.99, "860cal", "assets/images/menu7.jpg", 1);
-
-INSERT INTO Entrees (name, description, price, tags, image, MenuId) VALUES ("Porterhouse Steak", "Got the biggest steak in the game wearing our name! Our 22 oz porterhouse combines a bone-in strip and a tender filet into one thick cut.", 17.99, "1250cal", "assets/images/menu8.jpg", 1);
+INSERT INTO Entrees (name, description, price, tags, image, MenuId) VALUES ("Herb Grilled Salmon", "Grilled, bourbon-glazed salmon over rice", 12.99, "860cal", "https://images.pexels.com/photos/3655916/pexels-photo-3655916.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 1);
 
 
 
-INSERT INTO Drinks (name, description, price, tags, image, MenuId) VALUES ("Three Dons", "Don Julio Blanco, Reposado and Añejo with triple sec and all-natural sweet and sour.", 12.99, "450cal", "assets/images/menu9.jpg", 1);
 
-INSERT INTO Drinks (name, description, price, tags, image, MenuId) VALUES ("Old Fashioned", "Single Barrel Bourbon, Italian Amarena cherry, orange wedge and Angostura bitters.", 10.99, "160", "assets/images/menu10.jpg", 1);
+INSERT INTO Drinks (name, description, price, tags, image, MenuId) VALUES ("Mohito", "White rum, mint, and a drizzle of lime", 12.99, "450cal", "https://images.pexels.com/photos/4784/alcohol-bar-party-cocktail.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 1);
 
-INSERT INTO Drinks (name, description, price, tags, image, MenuId) VALUES ("Golden Ale", "A clean and crisp golden ale that pairs perfectly with our menu.", 6.99, "230cal", "assets/images/menu11.jpg", 1);
+INSERT INTO Drinks (name, description, price, tags, image, MenuId) VALUES ("Earl Gray Martini", "Tea leaves, lemon juice, white sugar and gin", 10.99, "160", "https://images.pexels.com/photos/613037/pexels-photo-613037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 1);
+
+INSERT INTO Drinks (name, description, price, tags, image, MenuId) VALUES ("Fruity Cooler", "Patron mixed with our signature fruit juice", 6.99, "230cal", "https://images.pexels.com/photos/452737/pexels-photo-452737.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 1);
 
 INSERT INTO Drinks (name, description, price, tags, image, MenuId) VALUES ("Signature Sangria", "A blend of chilled wine, fresh fruit and fruit juices. The ultimate refresher.", 12.99, "420cal", "assets/images/menu12.jpg", 1);
 
